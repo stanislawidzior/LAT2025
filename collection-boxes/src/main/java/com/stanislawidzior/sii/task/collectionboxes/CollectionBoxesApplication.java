25478efe3227fc6ca9c010a00ecf408c5fe5e3a2
stanidzior <stanosik123@gmail.com>
2025-05-10 14:59:24 +0200

    project structure

---
+++ collection-boxes/src/main/java/com/stanislawidzior/sii/task/collectionboxes/CollectionBoxesApplication.java
@@ -0,0 +1,13 @@
+package com.stanislawidzior.sii.task.collectionboxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionBoxesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionBoxesApplication.class, args);
+    }
+
+}
