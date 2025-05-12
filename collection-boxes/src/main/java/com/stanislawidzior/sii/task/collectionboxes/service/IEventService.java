916086bd98b6a2bef271d9a4026cc5f4b0e7a271
stanidzior <stanosik123@gmail.com>
2025-05-12 13:40:36 +0200

    change return type in createEvent to Long

--- collection-boxes/src/main/java/com/stanislawidzior/sii/task/collectionboxes/service/IEventService.java
+++ collection-boxes/src/main/java/com/stanislawidzior/sii/task/collectionboxes/service/IEventService.java
@@ -3,6 +3,6 @@ package com.stanislawidzior.sii.task.collectionboxes.service;
 import com.stanislawidzior.sii.task.collectionboxes.dtos.EventDTO;
 
 public interface IEventService {
-    public int createEvent(EventDTO dto);
+    Long createEvent(EventDTO dto);
 
 }
